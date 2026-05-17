3a8d04c9f580937ed69f8e02b33e2983ee43714c
Casey Link <unnamedrambler@gmail.com>
2026-05-17 15:26:58 +0200

    modernize nix development setup

--- build.clj
+++ build.clj
@@ -1,28 +1,49 @@
 (ns build
   (:require [clojure.tools.build.api :as b]
             [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clojure.string :as str]))
 
 (def project (-> (edn/read-string (slurp "deps.edn")) :aliases :neil :project))
-(def cwd (-> (java.io.File. ".")  .getCanonicalFile .getName))
-(def rev (str/trim (b/git-process {:git-args "rev-parse HEAD"})))
 (def lib (:name project))
 (def version (:version project))
 (def license-id (-> project :license :id))
 (def license-file (or (-> project :license :file) "LICENSE"))
 (def description (:description project))
-(def repo-url-prefix (:url project (-> (b/git-process {:git-args "remote get-url origin"})
-                                       (str/trim)
-                                       (str/replace #"\.git$" ""))))
-(def scm (:scm project "git"))
+
+(defn- git-origin-url []
+  (try
+    (some-> (b/git-process {:git-args "remote get-url origin"})
+            str/trim
+            (str/replace #"\.git$" ""))
+    (catch Exception _
+      nil)))
+
+(defn- git-rev []
+  (or (some-> (System/getenv "GIT_REV")
+              str/trim
+              not-empty)
+      (some-> (b/git-process {:git-args "rev-parse HEAD"})
+              str/trim
+              not-empty)))
+
+(def rev (git-rev))
+(def repo-url-prefix (or (:url project) (git-origin-url)))
 (assert lib ":name must be set in deps.edn under the :neil alias")
 (assert version ":version must be set in deps.edn under the :neil alias")
 (assert description ":description must be set in deps.edn under the :neil alias")
 (assert license-id "[:license :id] must be set in deps.edn under the :neil alias")
+(assert rev "Either GIT_REV must be set or git rev-parse HEAD must succeed")
+(assert repo-url-prefix "Either :url must be set in deps.edn under the :neil alias or git remote origin must exist")
 (def class-dir "target/classes")
 (def basis_ (delay (b/create-basis {:project "deps.edn"})))
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn- existing-paths [paths]
+  (->> paths
+       (filter #(.exists (io/file %)))
+       vec))
 
 (defn permalink [subpath]
   (str repo-url-prefix "/blob/" rev "/" subpath))
@@ -42,16 +63,16 @@
                 :lib lib
                 :version version
                 :basis @basis_
-                :src-dirs ["src"]
+                :src-dirs (existing-paths ["src"])
                 :pom-data [[:description description]
                            [:url repo-url-prefix]
                            [:licenses
                             [:license
                              [:name license-id]
                              [:url (permalink license-file)]]]
-                           (conj (url->scm repo-url-prefix) [:tag (str "v" version)])]})
+                           (conj (url->scm repo-url-prefix) [:tag rev])]})
 
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {:src-dirs (existing-paths ["src" "resources"])
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
@@ -66,7 +87,7 @@
 
 (defn uber [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src" "resources"] :target-dir class-dir})
+  (b/copy-dir {:src-dirs (existing-paths ["src" "resources"]) :target-dir class-dir})
   (b/compile-clj {:basis @basis_
                   :src-dirs ["src"]
                   :class-dir class-dir
--- flake.nix
+++ flake.nix
@@ -1,40 +1,165 @@
 {
   description = "dev env for datahike-sqlite";
+
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
-    flakelight.url = "github:nix-community/flakelight";
-    flakelight.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devenv.url = "https://flakehub.com/f/ramblurr/nix-devenv/*";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs =
-    {
+    inputs@{
       self,
-      flakelight,
+      devenv,
+      devshell,
       ...
     }:
-    flakelight ./. {
+    devenv.lib.mkFlake ./. {
+      inherit inputs;
+      withOverlays = [
+        devshell.overlays.default
+        devenv.overlays.default
+      ];
+      packages = {
+        default =
+          pkgs:
+          let
+            jdk = "jdk25";
+            jdkPackage = pkgs.${jdk};
+            clojure = pkgs.clojure.override { jdk = jdkPackage; };
+            root = toString ./.;
+            gitRev =
+              if self ? rev then
+                self.rev
+              else if self ? dirtyRev then
+                self.dirtyRev
+              else
+                "dirty";
+            projectSrc = pkgs.lib.cleanSourceWith {
+              src = ./.;
+              filter =
+                path: _type:
+                let
+                  rel = pkgs.lib.removePrefix (root + "/") (toString path);
+                  base = builtins.baseNameOf path;
+                in
+                !(
+                  base == ".git"
+                  || rel == "result"
+                  || rel == ".tmuxb_session"
+                  || rel == ".nrepl-port"
+                  || pkgs.lib.hasPrefix ".cpcache/" rel
+                  || pkgs.lib.hasPrefix ".clj-kondo/.cache/" rel
+                  || pkgs.lib.hasPrefix ".clj-kondo/imports/" rel
+                  || pkgs.lib.hasPrefix ".clj-kondo/inline-configs/" rel
+                  || pkgs.lib.hasPrefix ".direnv/" rel
+                  || pkgs.lib.hasPrefix ".lsp/" rel
+                  || pkgs.lib.hasPrefix "bench/results/" rel
+                  || pkgs.lib.hasPrefix "bench/tmp/" rel
+                  || pkgs.lib.hasPrefix "extra/" rel
+                  || pkgs.lib.hasPrefix "target/" rel
+                  || pkgs.lib.hasPrefix "test-data/" rel
+                );
+            };
+            clojureLocker = devenv.clojure.mkLockfile {
+              inherit pkgs;
+              jdk = jdkPackage;
+              src = ./.;
+              lockfile = "./deps-lock.json";
+              extraPrepInputs = [ pkgs.git ];
+            };
+          in
+          pkgs.stdenv.mkDerivation {
+            pname = "datahike-sqlite";
+            version = "0.0.1";
+            src = projectSrc;
+            nativeBuildInputs = [
+              clojure
+              pkgs.clj-kondo
+              pkgs.cljfmt
+              pkgs.coreutils
+              pkgs.findutils
+              pkgs.git
+              jdkPackage
+            ];
+            GIT_REV = gitRev;
+            JAVA_HOME = jdkPackage.home;
+            buildPhase = ''
+              runHook preBuild
+
+              source ${clojureLocker.shellEnv}
+              export JAVA_HOME="${jdkPackage.home}"
+              export JAVA_CMD="${jdkPackage}/bin/java"
+              export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Djava.io.tmpdir=$TMPDIR -Djna.tmpdir=$TMPDIR"
+
+              clojure -Srepro -Xdeps prep :aliases '[:dev :test]'
+              clj-kondo --fail-level error --lint src test bench
+              clojure -Srepro -M:dev:test
+              cljfmt -v check src test bench
+              clojure -Srepro -T:build jar
+
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p "$out"
+              cp "$(find target -type f -name '*.jar' -print | head -n 1)" "$out/"
+
+              runHook postInstall
+            '';
+          };
+
+        locker =
+          pkgs:
+          let
+            jdk = "jdk25";
+            jdkPackage = pkgs.${jdk};
+            clojure = pkgs.clojure.override { jdk = jdkPackage; };
+            clojureLocker = devenv.clojure.mkLockfile {
+              inherit pkgs;
+              jdk = jdkPackage;
+              src = ./.;
+              lockfile = "./deps-lock.json";
+              extraPrepInputs = [ pkgs.git ];
+            };
+          in
+          clojureLocker.commandLocker ''
+            export HOME="$tmp/home"
+            export GITLIBS="$tmp/home/.gitlibs"
+            unset CLJ_CACHE CLJ_CONFIG XDG_CACHE_HOME XDG_CONFIG_HOME XDG_DATA_HOME
+
+            ${clojure}/bin/clojure -Srepro -Xdeps prep :aliases '[:dev :test]'
+            ${clojure}/bin/clojure -Srepro -P -M:dev:test
+            ${clojure}/bin/clojure -Srepro -P -M:dev:bench
+            ${clojure}/bin/clojure -Srepro -P -T:build jar
+          '';
+      };
       devShell =
         pkgs:
-        let
-          javaVersion = "25";
-          jdk = pkgs."jdk${javaVersion}";
-          clojure = pkgs.clojure.override { inherit jdk; };
-        in
-        {
+        pkgs.devshell.mkShell {
+          imports = [
+            devenv.capsules.base
+            devenv.capsules.clojure
+          ];
+          env = [
+            {
+              name = "JAVA_HOME";
+              value = pkgs.jdk25.home;
+            }
+          ];
           packages = [
-            clojure
-            jdk
-            pkgs.clojure-lsp
             pkgs.clj-kondo
             pkgs.cljfmt
-            pkgs.babashka
+            pkgs.clojure-lsp
             pkgs.git
+            pkgs.glib
+            pkgs.jdk25
+            pkgs.vips
+            self.packages.${pkgs.system}.locker
           ];
         };
-
-      flakelight.builtinFormatters = false;
-      formatters = pkgs: {
-        "*.nix" = "${pkgs.nixfmt}/bin/nixfmt";
-        "*.clj" = "${pkgs.cljfmt}/bin/cljfmt fix";
-      };
     };
 }
